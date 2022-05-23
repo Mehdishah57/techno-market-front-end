e72ae0e84c146ce962f4ff597c4e466277eee135
Mehdishah57 <mehdi55shah@gmail.com>
2022-05-23 09:07:09 +0500

    9:10 AM 5/23/2022

--- src/components/UserNav.jsx
+++ src/components/UserNav.jsx
@@ -1,19 +1,68 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import { useNavigate } from 'react-router-dom';
+import Box from "@mui/material/Box";
+import {UserContext} from "../global/UserContext";
+import socket from '../socket/socket';
 
 import "../styles/user-nav.scss";
 
 const UserNav = () => {
 	const navigate = useNavigate();
+
+	const [, setUser] = useContext(UserContext);
+
+	const handleLogout = () => {
+		navigate("/login");
+		setUser({});
+		socket.disconnect();
+	}
+
 	return (
-		<div className='user-nav-wrapper'>
-			<ul>
-				<li>Change Phone Number</li>
-				<li onClick={() => navigate("my-ads")}>View Your Ads</li>
-				<li>Favourite Ads</li>
-				<li>Coming Soon</li>
-			</ul>
-		</div>
+		<Box 
+			display='flex'
+			justifyContent='center'
+			alignItems='center'
+			flexDirection='column'
+			width='100%'
+			maxWidth="600px"
+			margin="10px"
+			borderRadius='10px'
+			border='1px solid black'
+		>
+			<Box 
+				onClick={()=>navigate("/profile/favourites")} 
+				width='100%' padding='10px' 
+				borderBottom='1px solid black'
+				className="usernav-menu-item"
+
+			>
+				Favourites
+			</Box>
+			<Box 
+				onClick={()=>navigate("/messages")} 
+				width='100%' padding='10px' 
+				borderBottom='1px solid black'
+				className="usernav-menu-item"
+			>
+				Messages
+			</Box>
+			<Box 
+				onClick={()=>navigate("/sell")} 
+				width='100%' padding='10px' 
+				borderBottom='1px solid black'
+				className="usernav-menu-item"
+			>
+				Post an ad
+			</Box>
+			<Box 
+				onClick={handleLogout} 
+				width='100%' padding='10px' 
+				borderBottom='1px solid black'
+				className="usernav-menu-item"
+			>
+				Logout
+			</Box>
+		</Box>
 	)
 }
 
